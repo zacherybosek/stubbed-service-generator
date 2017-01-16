28c3425bdb634136f3e0d10e70eb977a0e75d2b6
Zach Bosek <zrbosek@gmail.com>
2017-01-15 21:32:00 -0600

    add unit tests for response scenarios

--- src/test/java/com/zacherybosek/testing/stubbedservice/response/ResponseTest.java
+++ src/test/java/com/zacherybosek/testing/stubbedservice/response/ResponseTest.java
@@ -4,6 +4,13 @@ import com.zacherybosek.testing.stubbedservice.StubbedService;
 import org.junit.After;
 import org.junit.Assert;
 import org.junit.Test;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.util.LinkedMultiValueMap;
+import org.springframework.web.client.RestTemplate;
+
+import static org.junit.Assert.assertEquals;
 
 /**
  * Created by Zachery on 7/26/2016.
@@ -19,26 +26,91 @@ public class ResponseTest {
 
     @Test
     public void singleParamResponse() throws Exception {
-        Assert.fail();
+        //given
+        stubbedService.onRequestTo("/health")
+                .giveResponse("hello");
+        stubbedService.start();
+        RestTemplate restTemplate = new RestTemplate();
+
+        //when
+        ResponseEntity<String> response = restTemplate.getForEntity("http://localhost:9999/health", String.class);
+
+        //then
+        assertEquals("hello", response.getBody());
     }
 
     @Test
     public void twoParamResponse() throws Exception {
-        Assert.fail();
+        //given
+        stubbedService.onRequestTo("/health")
+                .giveResponse("hello", HttpStatus.PARTIAL_CONTENT);
+        stubbedService.start();
+        RestTemplate restTemplate = new RestTemplate();
+
+        //when
+        ResponseEntity<String> response = restTemplate.getForEntity("http://localhost:9999/health", String.class);
+
+        //then
+        assertEquals("hello", response.getBody());
+        assertEquals(HttpStatus.PARTIAL_CONTENT, response.getStatusCode());
     }
 
     @Test
     public void threeParamResponse() throws Exception {
-        Assert.fail();
+        //given
+        stubbedService.onRequestTo("/health")
+                .giveResponse("hello", MediaType.APPLICATION_JSON, HttpStatus.ACCEPTED);
+        stubbedService.start();
+        RestTemplate restTemplate = new RestTemplate();
+
+        //when
+        ResponseEntity<String> response = restTemplate.getForEntity("http://localhost:9999/health", String.class);
+
+        //then
+        assertEquals("hello", response.getBody());
+        assertEquals(HttpStatus.ACCEPTED, response.getStatusCode());
+        assertEquals(MediaType.APPLICATION_JSON_VALUE, response.getHeaders().getContentType().toString().split(";")[0]);
     }
 
     @Test
     public void multipleResponses() throws Exception {
-        Assert.fail();
+        //given
+        stubbedService.onRequestTo("/health")
+                .giveResponse("hello").giveResponse("world");
+        stubbedService.start();
+        RestTemplate restTemplate = new RestTemplate();
+
+        //when
+        ResponseEntity<String> response = restTemplate.getForEntity("http://localhost:9999/health", String.class);
+
+        //then the first response should be returned
+        assertEquals("hello", response.getBody());
+
+        //when again
+        response = restTemplate.getForEntity("http://localhost:9999/health", String.class);
+
+        //then the second response should be returned
+        assertEquals("world", response.getBody());
     }
 
     @Test
     public void defaultResponseCalledMultipleTimes() throws Exception {
-        Assert.fail();
+        //given
+        stubbedService.onRequestTo("/health")
+                .giveResponse("hello");
+        stubbedService.start();
+        RestTemplate restTemplate = new RestTemplate();
+
+        //when
+        ResponseEntity<String> response = restTemplate.getForEntity("http://localhost:9999/health", String.class);
+
+        //then
+        assertEquals("hello", response.getBody());
+
+        //when again
+        response = restTemplate.getForEntity("http://localhost:9999/health", String.class);
+
+        //then the result should be the same
+        assertEquals("hello", response.getBody());
     }
 }
